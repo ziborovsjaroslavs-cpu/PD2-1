ecb7703bec23916c12bc9be44d1ffb6a58c6f0d3
Jaroslavs <ziborovsjaroslavs@gmail.com>
2026-02-24 13:08:49 +0200

    Add files via upload

---
+++ Uzdevums1.py
@@ -0,0 +1,19 @@
+
+nodes = [
+"Srv-Web-01;192.168.1.10;15;UP",
+"Srv-DB-01;192.168.1.20;450;UP",
+"Srv-Backup;10.0.0.5;0;DOWN",
+"Workstation-A;192.168.1.105;5;UP",
+"Srv-Proxy-01;172.16.0.1;10;up",
+"Srv-Mail;10.0.0.10;120;UP",
+"Router-Core;192.168.1.1;2;UP",
+"Srv-Dev-01;192.168.2.50;500;UP",
+"Printer-Main;192.168.1.200;0;down",
+"Srv-Log;10.0.0.15;105;UP"
+]
+
+print(":")
+
+for node in nodes:
+    hostname = node.split(";")[0]
+    print(hostname) 
---
+++ Uzdevums2.py
@@ -0,0 +1,24 @@
+nodes = [
+"Srv-Web-01;192.168.1.10;15;UP",
+"Srv-DB-01;192.168.1.20;450;UP",
+"Srv-Backup;10.0.0.5;0;DOWN",
+"Workstation-A;192.168.1.105;5;UP",
+"Srv-Proxy-01;172.16.0.1;10;up",
+"Srv-Mail;10.0.0.10;120;UP",
+"Router-Core;192.168.1.1;2;UP",
+"Srv-Dev-01;192.168.2.50;500;UP",
+"Printer-Main;192.168.1.200;0;down",
+"Srv-Log;10.0.0.15;105;UP"
+]
+print(":")
+
+up_servers = []
+
+for node in nodes:
+    hostname, ip, latency, status = node.split(";")
+
+if status.lower() == "up":
+    up_servers.append(hostname)
+
+print("UP serveri:", up_servers)
+print("kopa_servers")
